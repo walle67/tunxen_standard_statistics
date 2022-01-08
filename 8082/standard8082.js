bf191fb54f8801a60d623665b928edecdd59ab01
klay <walle67@163.com>
2022-01-08 10:42:08 +0800

    init

---
+++ 8082/standard8082.js
@@ -0,0 +1,21 @@
+﻿
+function openTunsenStandard() {
+    if(!(navigator.platform.indexOf('Win') > -1 || navigator.platform.indexOf('Mac') > -1)) {
+    	window['aplus_queue'] = window['aplus_queue'] || [];
+    	var f = document.getElementsByTagName('script')[0];
+    	var j = document.createElement('script');
+    	j.async = true;
+    	j.id = 'beacon-aplus';
+    	j.src = 'https://d.alicdn.com/alilog/mlog/aplus/203467608.js';
+    	f.parentNode.insertBefore(j, f);
+
+    	//集成应用的appKey
+    	aplus_queue.push({
+        action: 'aplus.setMetaInfo',
+        arguments: ['appKey', '6146af442a91a03cef4d51ba']
+    	});
+
+    	appRouter('com.tunxen.standard', 'TunXenStandard')
+    }
+}
+	   
