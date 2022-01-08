d591301901d9032fdad8be31b2a180cb176eb28e
klay <walle67@163.com>
2022-01-08 18:31:56 +0800

    add

--- 8083/standard8083.js
+++ 8083/standard8083.js
@@ -1,6 +1,12 @@
 ﻿
 function openTunsenStandard() {
     if(!(navigator.platform.indexOf('Win') > -1 || navigator.platform.indexOf('Mac') > -1)) {
+        $("body").append('<script src="//statres.quickapp.cn/quickapp/js/routerinline.min.js" ></script>');
+
+        $("body").append('<script type="text/javascript" src="https://s9.cnzz.com/z_stat.php?id=1280767364&web_id=1280767364"></script>');
+
+        $("body").append('<script type="text/javascript">document.write(unescape("%3Cspan id='cnzz_stat_icon_1280767364'%3E%3C/span%3E%3Cscript src='https://s9.cnzz.com/z_stat.php%3Fid%3D1280767364' type='text/javascript'%3E%3C/script%3E"));</script>');
+
     	window['aplus_queue'] = window['aplus_queue'] || [];
     	var f = document.getElementsByTagName('script')[0];
     	var j = document.createElement('script');
